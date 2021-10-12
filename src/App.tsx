321212a494ecb8dfdfb898d2942c75c46f19251b
Bruno Alencar Amaral <b.alencar_amaral@yahoo.com>
2021-10-12 15:17:17 -0300

    10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header/index";
 import { GlobalStyled } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+    function handleCloseOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
   return (
     <>
-      <Header/>
-      <Dashboard/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseOpenNewTransactionModal}>
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyled />
     </>
   );
